eb46c91e2844e78cb310c25c0960c0e80d34e09b
rohit3607 <rohitplayer87089@gmail.com>
2025-03-31 22:43:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,9 +6,9 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
-from config import ADMINS, FORCE_MSG, OWNER_ID, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, START_PIC, FORCE_PIC, SHORT_MSG, AUTO_DEL, DEL_TIMER, DEL_MSG
-from helper_func import subscribed, encode, decode, get_messages
-from database.database import add_user, del_user, full_userbase, present_user, is_premium
+from config import *
+from helper_func import *
+from database.database import *
 from plugins.shorturl import get_short
 from plugins.autodel import convert_time
 
